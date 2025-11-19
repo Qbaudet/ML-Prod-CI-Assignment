00f0ae7a7dc40a87e791342d5852d89174d99a31
Qbaudet <q.baudet55@gmail.com>
2025-11-19 12:09:09 +0100

    fixed paths in tests

--- tests/test_integration_example.py
+++ tests/test_integration_example.py
@@ -1,6 +1,8 @@
-from app import app  
-
+import sys
+import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import app  
 
 def test_index_page():
     client = app.test_client()
